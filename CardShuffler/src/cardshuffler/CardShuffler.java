93f2af40fef03bc83166fcdb71987daca6187f49
kustiki321 <kustiki321@gmail.com>
2018-09-12 21:16:24 -0400

    Add files via upload

---
+++ CardShuffler/src/cardshuffler/CardShuffler.java
@@ -0,0 +1,80 @@
+package cardshuffler;
+
+
+class Card
+{
+    private int value;
+    private char suit;
+    public Card(int value, char suit)
+    {
+        this.value = value;
+        this.suit = suit;
+    }
+    public int getValue()
+    {
+        return this.value;
+    }
+    public char getSuit()
+    {
+        return this.suit;
+    }
+    public String toString()
+    {
+        String value1 = Integer.toString(value);
+        switch (value) {
+            case 1:
+                value1 = "A";
+                break;
+            case 11:
+                value1 = "J";
+                break;
+            case 12:
+                value1 = "Q";
+                break;
+            case 13:
+                value1 = "K";
+                break;
+            default:
+                value1 = value1;
+                break;
+        }
+        String suit1 = String.valueOf(suit);
+        return String.format(value1 + suit1);
+    }
+    public boolean equals(Object o)
+    {
+        if (o == this)
+        {
+            return true;
+        }
+        if (!(o instanceof Card))
+        {
+            return false;
+        }
+        Card c1 = (Card)o;
+        Card c2 = (Card)o;
+        Card c3 = (Card)o;
+        return String(c1).equals(c2);
+    }
+}
+public class CardShuffler
+{
+    public static void main(String[] args)
+    {
+        Card c1 = new Card(11, 'c');
+        Card c2 = new Card(10, 'd');
+        Card c3 = new Card(11, 'c');
+        System.out.println(c1);
+        System.out.println(c3);
+        if (c1 == c3)
+        {
+        System.out.println("Equal");
+        }
+        else
+        {
+        System.out.println("Not Equal");
+        }
+    }
+}
+
+
